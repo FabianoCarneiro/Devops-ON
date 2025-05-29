eee5d03a068939557859510afb13b248cc6235f0
FabianoCarneiro <FabianoCarneiro>
2025-05-28 21:52:37 -0300

    Disable CSRF protection line in SecurityConfiguration for improved security practices

--- src/main/java/com/example/demo/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/demo/configuration/SecurityConfiguration.java
@@ -30,7 +30,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable();
+    //http.csrf().disable();
     // Remova esta linha:
     // http.headers().disable();
 
