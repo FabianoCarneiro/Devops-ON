531d162f3b2354fba3739a0a95ad113c948f250f
FabianoCarneiro <37809484+FabianoCarneiro@users.noreply.github.com>
2025-10-20 21:20:12 -0300

    Modify contextLoads test and remove endpoint tests
    Updated the contextLoads test to assert false and removed multiple endpoint tests.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -18,53 +18,8 @@ class DemoApplicationTests {
 
     @Test
     void contextLoads() {
-        assert(true);
+        assert(false);
     }
 
-    @Test
-    void healthCheckEndpointShouldReturnOk() throws Exception {
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("HEALTH CHECK OK!"));
-    }
-
-    @Test
-    void publicEndpointShouldReturnOk() throws Exception {
-        mockMvc.perform(get("/public"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("This is public endpoint"));
-    }
-
-    @Test
-    void devopsEndpointShouldReturnZumbi() throws Exception {
-        mockMvc.perform(get("/devops"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("Zumbi"));
-    }
-
-    @Test
-    void autoglassEndpointShouldReturnUrl() throws Exception {
-        mockMvc.perform(get("/autoglass"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("https://www.autoglassonline.com.br/"));
-    }
-
-    @Test
-    void whatIsTheTimeEndpointShouldReturnDate() throws Exception {
-        mockMvc.perform(get("/what-is-the-time"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(org.hamcrest.Matchers.notNullValue()));
-    }
-
-    @Test
-    void securedEndpointShouldRequireAuthentication() throws Exception {
-        mockMvc.perform(get("/secured"))
-                .andExpect(status().isUnauthorized());
-    }
-
-    @Test
-    void securedAdminEndpointShouldRequireAuthentication() throws Exception {
-        mockMvc.perform(get("/secured-admin"))
-                .andExpect(status().isUnauthorized());
-    }
+ 
 }
